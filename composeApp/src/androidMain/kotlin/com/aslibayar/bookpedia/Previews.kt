9b212314956c71c547b91b1a5469babddfdb009e
aslibayar <aslibyr00@gmail.com>
2025-10-12 19:29:07 +0300

    Refactor book data initialization and update string resource for favorites message

--- composeApp/src/androidMain/kotlin/com/aslibayar/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/aslibayar/bookpedia/Previews.kt
@@ -4,9 +4,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.aslibayar.bookpedia.book.domain.Book
 import com.aslibayar.bookpedia.book.presentation.book_list.BookListScreen
 import com.aslibayar.bookpedia.book.presentation.book_list.BookListState
-import com.aslibayar.bookpedia.book.presentation.book_list.books
 import com.aslibayar.bookpedia.book.presentation.book_list.components.SearchBar
 
 @Preview(backgroundColor = 0xFFFFFF, showBackground = true)
@@ -20,6 +20,21 @@ fun SearchBarPreview(modifier: Modifier = Modifier) {
     )
 }
 
+private val books = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book Title $it",
+        authors = listOf("Aslı BAYAR"),
+        description = "This is a description of Book $it. ".repeat(10),
+        imageUrl = "https://test/200",
+        languages = emptyList(),
+        publishYear = "2023",
+        avgRating = 4.7676,
+        ratingCount = 100,
+        pages = 300,
+        numEditions = 5
+    )
+}
 
 @Preview
 @Composable
--- composeApp/src/commonMain/kotlin/com/aslibayar/bookpedia/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/aslibayar/bookpedia/book/presentation/book_list/BookListState.kt
@@ -12,19 +12,3 @@ data class BookListState(
     val errorText: UiText? = null
 )
 
-
-val books = (1..100).map {
-    Book(
-        id = it.toString(),
-        title = "Book Title $it",
-        imageUrl = "https://test/200",
-        authors = listOf("Aslı BAYAR"),
-        description = "Book Description",
-        languages = emptyList(),
-        publishYear = "2023",
-        avgRating = 4.5,
-        ratingCount = 100,
-        pages = 300,
-        numEditions = 5
-    )
-}
