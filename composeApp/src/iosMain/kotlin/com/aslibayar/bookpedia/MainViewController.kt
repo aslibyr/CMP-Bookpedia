f95a7fededf8d1024b120c0be128ac35f734d898
aslibayar <aslibyr00@gmail.com>
2025-11-25 08:33:47 +0300

    Add Ktor client engine import and use remember in MainViewController

--- composeApp/src/iosMain/kotlin/com/aslibayar/bookpedia/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/aslibayar/bookpedia/MainViewController.kt
@@ -1,6 +1,8 @@
 package com.aslibayar.bookpedia
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import io.ktor.client.engine.darwin.Darwin
 
 fun MainViewController() = ComposeUIViewController {
     App(engine = remember {
